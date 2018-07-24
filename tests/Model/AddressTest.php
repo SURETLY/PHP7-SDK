3d1372a66f1ee1a42d1d62dff716dcf8671248e6
fafnur <fafnur@yandex.ru>
2018-07-24 16:24:00 +0700

    update address fields

---
+++ tests/Model/AddressTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Suretly\LenderApi\Tests\Model;
+
+use PHPUnit\Framework\TestCase;
+use Suretly\LenderApi\Model\Address;
+
+/**
+ * Class AddressTest
+ * @package Suretly\LenderApi\Tests\Model
+ */
+class AddressTest extends TestCase
+{
+    /**
+     * @dataProvider paramsProvider
+     *
+     * @param $params
+     * @param $addressParams
+     * @throws \JsonMapper_Exception
+     */
+    public function testGetAddress($params, $addressParams)
+    {
+        /** @var Address $address */
+        $address = $this->getAddress($addressParams);
+
+        $this->assertNotEmpty($address);
+    }
+
+    /**
+     * Params provider
+     *
+     * @return array
+     */
+    public function paramsProvider()
+    {
+        return [
+            [
+                'params' => [
+                    'id' => '59ca100acea0997574cef785',
+                    'token' => '317',
+                    'server' => 'develop'
+                ],
+                'address' => [
+                    'country' => 'Russia',
+                    'zip' => '630093',
+                    'area' => 'Russia',
+                    'city' => 'Novosibirsk',
+                    'street' => 'Serebrennikovskaya',
+                    'house' => '29',
+                    'building' => '1',
+                    'flat' => '10'
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * @param $address
+     * @return mixed
+     * @throws \JsonMapper_Exception
+     */
+    private function getAddress($address)
+    {
+        $jsonMapper = new \JsonMapper();
+
+        return $jsonMapper->map(json_decode(json_encode($address)), new Address());
+    }
+}
